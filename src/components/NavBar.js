31c6e42736fa9eb018e138281bf2522a28fbb1de
Sunshine Moua <sunshinemoua@Sunshines-MacBook-Pro.local>
2022-12-30 20:43:35 -0800

    added css media queries for navbar responsiveness

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,48 +5,65 @@ import classes from "./NavBar.module.css";
 const NavBar = () => {
   return (
     <div className={classes.navbar}>
-      <NavLink
-        to="/"
-        className={({ isActive }) => (isActive ? classes.active : classes.link)}
-      >
-        Home
+      <NavLink to="/" className={classes["navbar-logo"]}>
+        LOGO
       </NavLink>
-      <NavLink
-        to="/films"
-        className={({ isActive }) => (isActive ? classes.active : classes.link)}
-      >
-        Films
-      </NavLink>
-      <NavLink
-        to="/planets"
-        className={({ isActive }) => (isActive ? classes.active : classes.link)}
-      >
-        Planets
-      </NavLink>
-      <NavLink
-        to="/species"
-        className={({ isActive }) => (isActive ? classes.active : classes.link)}
-      >
-        Species
-      </NavLink>
-      <NavLink
-        to="/people"
-        className={({ isActive }) => (isActive ? classes.active : classes.link)}
-      >
-        People
-      </NavLink>
-      <NavLink
-        to="/vehicles"
-        className={({ isActive }) => (isActive ? classes.active : classes.link)}
-      >
-        Vehicles
-      </NavLink>
-      <NavLink
-        to="/spaceships"
-        className={({ isActive }) => (isActive ? classes.active : classes.link)}
-      >
-        Spaceships
-      </NavLink>
+
+      <a href="#" className={classes["toggle-button"]}>
+        <span className={classes.bar}></span>
+        <span className={classes.bar}></span>
+        <span className={classes.bar}></span>
+      </a>
+      <div className={classes["navbar-links"]}>
+        <NavLink
+          to="/films"
+          className={({ isActive }) =>
+            isActive ? classes.active : classes.link
+          }
+        >
+          Films
+        </NavLink>
+        <NavLink
+          to="/planets"
+          className={({ isActive }) =>
+            isActive ? classes.active : classes.link
+          }
+        >
+          Planets
+        </NavLink>
+        <NavLink
+          to="/species"
+          className={({ isActive }) =>
+            isActive ? classes.active : classes.link
+          }
+        >
+          Species
+        </NavLink>
+        <NavLink
+          to="/people"
+          className={({ isActive }) =>
+            isActive ? classes.active : classes.link
+          }
+        >
+          People
+        </NavLink>
+        <NavLink
+          to="/vehicles"
+          className={({ isActive }) =>
+            isActive ? classes.active : classes.link
+          }
+        >
+          Vehicles
+        </NavLink>
+        <NavLink
+          to="/spaceships"
+          className={({ isActive }) =>
+            isActive ? classes.active : classes.link
+          }
+        >
+          Spaceships
+        </NavLink>
+      </div>
     </div>
   );
 };
